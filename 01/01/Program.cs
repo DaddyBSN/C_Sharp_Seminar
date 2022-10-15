80e95d693f9f077a29af1043742cf76c4a5a7ef7
Daddy <sergoffan@mail.ru>
2022-10-16 00:47:55 +0300

    repair

---
+++ 01/01/Program.cs
@@ -0,0 +1,30 @@
+﻿//
+//
+Console.WriteLine("Enter number a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Enter number b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+int max = 0;
+int min = 0;
+
+if (a < b)
+{
+    min = a;
+    max = b;
+    Console.WriteLine("Min = " + min);
+    Console.WriteLine("Max = " + max);
+}
+else
+if (b < a)
+{
+    min = b;
+    max = a;
+    Console.WriteLine("Min = " + min);
+    Console.WriteLine("Max = " + max);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
